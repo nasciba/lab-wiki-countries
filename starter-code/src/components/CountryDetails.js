514d21057d3f43909440f8e0450698c734b837a1
amandanasciba <amandanasciba@gmail.com>
2020-05-07 13:37:38 -0300

    lab finished

--- starter-code/src/components/CountryDetails.js
+++ starter-code/src/components/CountryDetails.js
@@ -9,7 +9,6 @@ class CountryDetails extends Component {
         super(props);
         const { params } = this.props.match;
         const country = this.getCountry(params.id);
-        
         this.state = {
             countries: countries,
             country: country,
@@ -27,48 +26,50 @@ class CountryDetails extends Component {
     componentDidMount() {
         const { params } = this.props.match;
         const newCountry = this.getCountry(params.id);
-        this.setState({country : newCountry});
-        
+        this.setState({ country: newCountry });
+
     }
 
     render() {
-        console.log('oi')
-        const countryName = this.state.country.name && this.state.country.name.common 
+        const countryName = this.state.country.name && this.state.country.name.common
 
         return (
             <div>
-            <div>
-            <div className="col-7">
-                <h1>{this.state.country.flag} {countryName} </h1>
-                <table class="table">
-                    <thead></thead>
-                    <tbody>
-                        <tr>
-                            <td style={{ width: '30%' }}>Capital</td>
-                            <td>{this.state.country.capital}</td>
-                        </tr>
-                        <tr>
-                            <td>Area</td>
-                            <td>{this.state.country.area} km
+                <div>
+                    <div className="col-7">
+                        <h1>{this.state.country.flag} {countryName} </h1>
+                        <table class="table">
+                            <thead></thead>
+                            <tbody>
+                                <tr>
+                                    <td style={{ width: '30%' }}>Capital</td>
+                                    <td>{this.state.country.capital}</td>
+                                </tr>
+                                <tr>
+                                    <td>Area</td>
+                                    <td>{this.state.country.area} km
                     <sup>2</sup>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Borders</td>
-                            <td>
-                                <ul>{this.state.country.borders ? this.state.country.borders.map((border, index) => {
-                                    return (<li key={index}><Link to={`/countrydetail/${border}`}>{border}</Link></li>)
-                                    }) : null }
-                                    
-       
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Borders</td>
+                                    <td>
+                                        <ul>{this.state.country.borders ? this.state.country.borders.map((border, index) => {
+                                            countries.find(country => {
+                                                return country.ccn === border
+                                            })
+                                            return (<li key={index}><Link to={`/countrydetail/${border}`}>{border}</Link></li>)
+                                        }) : null}
 
-                                </ul>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            </div>
+
+
+                                        </ul>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         )
     }
